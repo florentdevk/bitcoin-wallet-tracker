35fa21c3580f1c06dacca972c739e0b0147e75eb
florentdevk <florentdevk@gmail.com>
2026-06-25 19:01:05 +0200

    feat: remove unused alert types and clean enum

--- src/Enum/AlertType.php
+++ src/Enum/AlertType.php
@@ -6,8 +6,6 @@ namespace App\Enum;
 
 enum AlertType: string
 {
-    case Incoming = 'incoming';
-    case Outgoing = 'outgoing';
     case BalanceAbove = 'balance_above';
     case BalanceBelow = 'balance_below';
 }
